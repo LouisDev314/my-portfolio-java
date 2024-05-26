63a9861ac50e403d432aee49e1b69f80b164c5a7
Louis <79896040+LouisDev314@users.noreply.github.com>
2024-05-26 12:35:55 +0800

    chore: init spring boot

---
+++ MyPortfolio/src/test/java/com/LouisDev314/SpringBoot/MyPortfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LouisDev314.SpringBoot.MyPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
